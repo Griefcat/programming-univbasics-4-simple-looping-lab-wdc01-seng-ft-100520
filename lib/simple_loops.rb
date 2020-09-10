b2fe661c20c3cf6493b2dabcfc092962080fee0c
Anne Nardolilli <anardolilli@gmail.com>
2020-09-10 01:52:23 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -24,13 +24,13 @@ def output_array(array)
 end 
 
 def return_string_array(array)
-  counter = 0 
-  new_array =[]
-  while counter < array.length do 
-    new_array.push(array[counter].to_s 
-    counter += 1 
-  end 
+  count = 0
+  new_array = []
+  while count < array.length do
+    new_array.push(array[count].to_s) # OR new_array << array[count].to_s
+    count += 1
+  end
   new_array
-end 
+end
 
   
