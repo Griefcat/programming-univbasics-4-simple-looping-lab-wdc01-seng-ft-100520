425bedf78cdc00ed29fb79978ddc383ad64b83a8
Anne Nardolilli <anardolilli@gmail.com>
2020-09-10 01:42:59 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,3 +1,19 @@
+def loop_message_five_times(string)
+ count = 0
+ while count < 5 do
+   puts string 
+   count += 1 
+  end 
+end 
+
+
+def loop_message_n_times(string, integer)
+  count = 0 
+  while count < integer 
+    puts string 
+    count += 1 
+  end 
+end 
 
 def output_array(array)
   counter = 0 
