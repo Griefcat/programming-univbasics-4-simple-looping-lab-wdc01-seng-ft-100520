b0c425027553d854b3ed30c1c91471c1e6be1a95
Anne Nardolilli <anardolilli@gmail.com>
2020-09-10 01:40:34 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -16,5 +16,14 @@ def loop_message_n_times(string, integer)
 end 
 
 def output_array(array)
+  counter = 0 
+  while counter < array.length do 
+    puts array[counter]
+    counter += 1 
+  end 
+end 
+
+love = ["Juan", "is", "my", "husband"]
+output_array(love)
 
   
